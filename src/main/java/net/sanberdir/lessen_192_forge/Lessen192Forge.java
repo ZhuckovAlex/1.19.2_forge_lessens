a7c7a1df1124c62085af60ff358a5d730b8ea9cc
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2024-06-20 12:47:36 +0300

    lesson_5

--- src/main/java/net/sanberdir/lessen_192_forge/Lessen192Forge.java
+++ src/main/java/net/sanberdir/lessen_192_forge/Lessen192Forge.java
@@ -20,10 +20,15 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+<<<<<<< Updated upstream
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+=======
+import net.sanberdir.lessen_192_forge.blocks.InitBlocks;
+import net.sanberdir.lessen_192_forge.items.InitItems;
+>>>>>>> Stashed changes
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Lessen192Forge.MODID)
@@ -43,7 +48,12 @@ public class Lessen192Forge
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+<<<<<<< Updated upstream
 
+=======
+        InitItems.register(modEventBus);
+        InitBlocks.register(modEventBus);
+>>>>>>> Stashed changes
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
