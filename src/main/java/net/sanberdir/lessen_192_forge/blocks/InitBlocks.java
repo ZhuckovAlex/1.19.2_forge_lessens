816c69dca7ac7980ba9fc36ac0cc52849fa6deb7
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2024-06-21 14:06:22 +0300

    lesson_7

---
+++ src/main/java/net/sanberdir/lessen_192_forge/blocks/InitBlocks.java
@@ -0,0 +1,69 @@
+package net.sanberdir.lessen_192_forge.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.sanberdir.lessen_192_forge.Lessen192Forge;
+import net.sanberdir.lessen_192_forge.blocks.custom.FlameBlock;
+import net.sanberdir.lessen_192_forge.blocks.custom.FlameBlockRotate;
+import net.sanberdir.lessen_192_forge.items.InitItems;
+import net.sanberdir.lessen_192_forge.tab.ModCreativeTab;
+
+import java.util.function.Supplier;
+
+public class InitBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, Lessen192Forge.MODID);
+
+    public static final RegistryObject<Block> CUSTOM_PLANKS = registerBlock("custom_planks",
+            () -> new FlameBlock(BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(4.0F, 5.0F)
+                    .sound(SoundType.STEM)), ModCreativeTab.SWEET_TAB);
+
+
+    public static final RegistryObject<Block> CUSTOM_WOOD = registerBlock("custom_wood",
+            () -> new FlameBlockRotate(BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(4.0F, 5.0F)
+                    .sound(SoundType.STEM)), ModCreativeTab.SWEET_TAB);
+    public static final RegistryObject<Block> CUSTOM_LOG = registerBlock("custom_log",
+            () -> new FlameBlockRotate(BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(4.0F, 5.0F)
+                    .sound(SoundType.STEM)), ModCreativeTab.SWEET_TAB);
+
+    public static final RegistryObject<Block> STRIPPED_CUSTOM_LOG = registerBlock("stripped_custom_log",
+            () -> new FlameBlockRotate(BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(4.0F, 5.0F)
+                    .sound(SoundType.STEM)), ModCreativeTab.SWEET_TAB);
+    public static final RegistryObject<Block> STRIPPED_CUSTOM_WOOD = registerBlock("stripped_custom_wood",
+            () -> new FlameBlockRotate(BlockBehaviour.Properties.of(Material.WOOD, MaterialColor.WOOD).strength(4.0F, 5.0F)
+                    .sound(SoundType.STEM)), ModCreativeTab.SWEET_TAB);
+    
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab) {
+        return InitItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
+    private static <T extends Block> RegistryObject<T> registerBlockWithoutBlockItem(String name, Supplier<T> block) {
+        return BLOCKS.register(name, block);
+    }
+    private static boolean always(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return true;
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
