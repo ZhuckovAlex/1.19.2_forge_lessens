8bbbc1a5031210398ae12e04ac1d79e84c9fbd86
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2024-06-21 14:07:35 +0300

    lesson_7

--- src/main/java/net/sanberdir/lessen_192_forge/Lessen192Forge.java
+++ src/main/java/net/sanberdir/lessen_192_forge/Lessen192Forge.java
@@ -1,14 +1,5 @@
 package net.sanberdir.lessen_192_forge;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,10 +11,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
+import net.sanberdir.lessen_192_forge.blocks.InitBlocks;
+import net.sanberdir.lessen_192_forge.items.InitItems;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Lessen192Forge.MODID)
@@ -37,13 +26,11 @@ public class Lessen192Forge
     public Lessen192Forge()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
+        InitItems.register(modEventBus);
+        InitBlocks.register(modEventBus);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
